a2535f3da6c8ec22aaecec66cf29bcc9292765a2
henil-ciccc <142459114+henil-ciccc@users.noreply.github.com>
2024-01-28 18:36:39 -0800

    Initialized Routes and Added Pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,36 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
 import Image from "next/image";
 import { useState } from "react";
 
 const Navbar = () => {
+	const pathname = usePathname();
+
 	const [isNavOpen, setIsNavOpen] = useState(false);
+
+	const navLinks = [
+		{ linkText: "Templates", routePath: "/templates" },
+		{ linkText: "Collections", routePath: "/collections" },
+		{ linkText: "Support", routePath: "/support" },
+	];
 
 	return (
 		<div className="bg-gray-900">
 			<header className="max-w-[1200px] mx-auto sm:flex sm:justify-between sm:px-4 sm:py-3">
 				<div className="flex items-center justify-between px-4 py-3 sm:p-0">
-					<div className="flex items-center text-white font-semibold text-2xl">
+					<Link
+						onClick={() => {
+							setIsNavOpen(false);
+						}}
+						href="/"
+						className="flex items-center text-white font-semibold text-2xl"
+					>
 						<Image priority src="/assets/rg_logo.png" width={65} height={65} alt="Readme Generator" />
 						<p className="hidden md:block">Readme Generator</p>
-					</div>
+					</Link>
 					<div className="sm:hidden me-2">
 						<button
 							onClick={() => {
@@ -33,24 +50,20 @@ const Navbar = () => {
 					</div>
 				</div>
 				<div className={`${isNavOpen ? "block px-5 pt-2 pb-4" : "hidden"} sm:flex sm:p-0 sm:items-center`}>
-					<a
-						href="#"
-						className="block px-2 py-2 mt-0  sm-mt-0 text-white font-semibold rounded hover:bg-gray-700"
-					>
-						Templates
-					</a>
-					<a
-						href="#"
-						className="block px-2 py-2 mt-2 md:mt-0 sm:ml-3 md:ml-6 text-white font-semibold rounded hover:bg-gray-700"
-					>
-						Collections
-					</a>
-					<a
-						href="#"
-						className="block px-2 py-2 mt-2 md:mt-0 sm:ml-3 md:ml-6 text-white font-semibold rounded hover:bg-gray-700"
-					>
-						Support
-					</a>
+					{navLinks.map((navLink, index) => (
+						<Link
+							onClick={() => {
+								setIsNavOpen(false);
+							}}
+							key={index}
+							className={`block px-2 py-2 mt-0 sm-mt-0 sm:ml-3 md:ml-6 text-white font-semibold rounded hover:bg-gray-700 link ${
+								pathname === navLink.routePath ? "bg-white text-gray-900" : ""
+							}`}
+							href={navLink.routePath}
+						>
+							{navLink.linkText}
+						</Link>
+					))}
 					<a
 						href="#"
 						className="block px-2 md:px-4 py-2 mt-2 md:mt-0 sm:ml-3 md:ml-8 text-white font-semibold rounded bg-green-600 hover:opacity-85"
